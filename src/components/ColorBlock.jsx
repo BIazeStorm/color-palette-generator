b541bb74bee4c8f6cd1ddaa882c61e598d7b655b
BIazeStorm <mikolaforwork@gmail.com>
2025-10-22 00:24:00 +0300

    feat: create Palette and ColorBlock component files

---
+++ src/components/ColorBlock.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ColorBlock(){
+    return (
+        <div className="ColorBlock">
+            <h2>#000000</h2>
+        </div>
+    );
+}
+
+export default ColorBlock;
---
+++ src/components/Palette.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ColorBlock from "./ColorBlock";
+
+function Palette() {
+    return (
+        <div className="Palette">
+            <ColorBlock />
+            <ColorBlock />
+            <ColorBlock />
+            <ColorBlock />
+            <ColorBlock />
+        </div>
+    );
+}
+
+export default Palette;

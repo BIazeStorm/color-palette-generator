67824f9067121ebb07e8438f3d23955ab4a2a13b
BIazeStorm <mikolaforwork@gmail.com>
2025-10-22 11:36:14 +0300

    feat: add colors state to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Palette from './components/Palette'
 import './App.css'
 
 function App() {
 
-  const staticColors = [
+  const [colors, setColors] = useState([
     { id: 1, hex: '#e63946' },
     { id: 2, hex: '#f1faee' },
     { id: 3, hex: '#a8dadc' },
     { id: 4, hex: '#457b9d' },
     { id: 5, hex: '#1d3557' },
-  ];
+  ]);
 
   return (
     <div className="App">
-      <Palette colors={staticColors} />
+      <Palette colors={colors} />
     </div>
   );
 }

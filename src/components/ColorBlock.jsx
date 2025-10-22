9f859cb3e90df43ec98b39854c6824ac42698871
BIazeStorm <mikolaforwork@gmail.com>
2025-10-22 22:41:46 +0300

    feat: add toggle lock button to ColorBlock

--- src/components/ColorBlock.jsx
+++ src/components/ColorBlock.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 function ColorBlock({ color }){
     const { id, hex, isLocked } = color;
-    
+
     return (
         <div className="ColorBlock" style={{ backgroundColor: hex }}>
+            <button className={`ColorBlock__lock-btn ${isLocked ? 'is-locked' : ''}`}>
+                {isLocked ? '🔒' : '🔓'}
+            </button>
             <h2>{hex}</h2>
         </div>
     );
